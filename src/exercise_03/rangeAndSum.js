cd28864a0718902d5ab11050bd9353592a0047a3
Lu <1625196521@qq.com>
2020-09-06 10:44:45 +0800

    exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,13 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const arry = [];
+  if (end - start > 0) {
+    for (let i = 0; i < end - start; i += 1) { arry.push(start + i); }
+  } else {
+    for (let i = 0; i < start - end; i += 1) { arry.push(start - i); }
+  }
+  return arry;
 }
 
 export function sum(...numbers) {
@@ -17,6 +22,16 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let total = 0;
+  if (numbers instanceof Array) {
+    for (let i = 0; i < numbers.length; i += 1) {
+      total += numbers[i];
+    }
+  } else {
+    const a = [...numbers];
+    for (let i = 0; i < a.length; i += 1) {
+      total += a[i];
+    }
+  }
+  return total;
 }
