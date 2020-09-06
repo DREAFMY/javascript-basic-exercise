81831423bfdee1f481386519bb498d9ed1720273
Lu <1625196521@qq.com>
2020-09-06 09:58:01 +0800

    exercise_02

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,15 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let i = 0;
+  if (!string) {
+    i = 0;
+  } else if (prediction) {
+    for (let j = 0; j < string.length; j += 1) {
+      if (prediction(string[j])) { i += 1; }
+    }
+  } else {
+    i = string.length;
+  }
+  return i;
 }
