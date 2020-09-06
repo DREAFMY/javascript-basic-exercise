81ff5fa9160f028437b17b0a8ba6d5fb4c00b037
Lu <1625196521@qq.com>
2020-09-06 11:33:18 +0800

    exercise_05

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,23 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
+  const myLink = {};
 
-  throw new Error('Please delete this line and implement the function');
+  function toLink(a, l) {
+    l.value = a[0];
+    if (a[1]) {
+      a.shift();
+      l.next = {};
+      toLink(a, l.next);
+    } else {
+      l.next = null;
+    }
+    return l;
+  }
+
+  if (!array) { throw new Error('Creating list from undefined array'); }
+  else if (array.length === 0) { throw new Error('Creating list from empty array'); }
+  else {
+    return toLink(array, myLink);
+  }
 }
