c820c852f06ec6f6a3f4efc2e171830075f5beba
Lu <1625196521@qq.com>
2020-09-06 11:54:00 +0800

    exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,19 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (!array) { throw new Error('Flatten undefined or null array'); }
+  else if (array.length === 0) { return []; }
+  else {
+    let a = [];
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] instanceof Array) {
+        array[i].map(it => {
+          a.push(it);
+        });
+      } else {
+        a.push(array[i]);
+      }
+    }
+    return a;
+  }
 }
